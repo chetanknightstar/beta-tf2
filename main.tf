00429cb979620223e82266bfafa8d8faebd3cafd
chetanknightstar <126722416+chetanknightstar@users.noreply.github.com>
2023-04-21 05:39:24 +0530

    code edit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   #ami           = "ami-064087b8d355e9051"
   #instance_type = "t3.micro"
   ami            =var.ami
-  instance       = var.instance_type
+  instance_type   = var.instance_type
 
   tags = {
     Name = "HelloWorld"
